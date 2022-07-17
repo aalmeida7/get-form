bc07506878a82e99f19d84933090e03799751c8c
Alessandro Almeida <87588664+aalmeida7@users.noreply.github.com>
2022-07-17 14:30:22 -0300

    Update mazagleads.js

--- mazagleads.js
+++ mazagleads.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
+    let url_string = window.location.href;
+    let param = new URL(url_string);
+    let id = param.searchParams.get("id"); //pega o value
     let url = window.location.href;
+    $("form").append("<input type='hidden' name='id' value='"+id+"'/>");
     $("form").append("<input type='hidden' name='url' value='"+url+"'/>");
     $("button").click(function() {
         let data = $("form").serializeArray();
