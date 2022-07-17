cd9a35540f56f3b75b9b002d3c76a93b17fef0a0
Alessandro Almeida <87588664+aalmeida7@users.noreply.github.com>
2022-07-17 14:17:41 -0300

    Delete mazagleads.js

--- mazagleads.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    let url = window.location.href;
-    $("form").append("<input type='hidden' name='url' value='"+url+"'/>");
-    $("button").click(function() {
-        let data = $("form").serializeArray();
-        $.post('http://localhost:3001/api/v1', data);                
-    });
-});
